7ee2d544d9714ed7c66acebc1facbf1447743cb1
hannah <yuhan.chi12@gmail.com>
2021-08-09 17:38:57 -0700

    modify actionPerformed

--- src/tetris/controllers/Controller.java
+++ src/tetris/controllers/Controller.java
@@ -104,7 +104,6 @@ public class Controller implements ActionListener {
 
     public void updateRecord() {
         int addRemoveLine = this.game.countFullLines();
-        System.out.printf("addRemoveLine: %d\n", addRemoveLine);
         this.removedLines += addRemoveLine;
         this.score += addRemoveLine * this.level * this.LevelRate;
         this.level = 1 + this.removedLines / this.scoreToLevel;
@@ -123,7 +122,6 @@ public class Controller implements ActionListener {
         // We reach here every time the alarm fires.
         // if not gameOver and not isPaused, we need to call models
         if (!this.game.isGameOver() && !this.isPaused()) {
-//            System.out.printf("finishedFalling %b\n", this.game.isFallingFinished());
             // if current piece is settled
             if (this.game.isFallingFinished()) {
                 this.updateRecord(); // remove full line and update records
